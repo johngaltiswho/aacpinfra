4bc35b582bdd8546781e959fab590060da711e9d
johngaltiswho <umamaheshwar@aacpinfra.com>
2017-11-25 22:28:08 +0530

    third Iteration

--- proloc.js
+++ proloc.js
@@ -1,7 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obmdhbHRpc3dobyIsImEiOiJjajNyZjl3eDYwMWJiMnBvNmlnd3pjcWJqIn0.b-03hU-DxQF21JQC7v5j8g';
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/johngaltiswho/cja9pkbuj1qeb2sro7a3i7aei'
+  style: 'mapbox://styles/johngaltiswho/cja9pkbuj1qeb2sro7a3i7aei',
+  center: [77.5946, 12.9716 ],
+  zoom: 4
 });
 
 map.on('click', function(e) {
@@ -17,7 +19,7 @@ map.on('click', function(e) {
 
   var popup = new mapboxgl.Popup({ offset: [0, -15] })
     .setLngLat(feature.geometry.coordinates)
-    .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+    .setHTML('<h3>' + feature.properties.Title + '</h3><p>' + feature.properties.Description + '</p>')
     .setLngLat(feature.geometry.coordinates)
     .addTo(map);
 });
