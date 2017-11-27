27bac465d02b698e9dd416e8a6c0eabddf6ecd1a
johngaltiswho <umamaheshwar@aacpinfra.com>
2017-11-27 16:06:37 +0530

    third iteration

--- proloc.js
+++ proloc.js
@@ -1,9 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obmdhbHRpc3dobyIsImEiOiJjajNyZjl3eDYwMWJiMnBvNmlnd3pjcWJqIn0.b-03hU-DxQF21JQC7v5j8g';
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/johngaltiswho/cja9pkbuj1qeb2sro7a3i7aei',
+  style: 'mapbox://styles/johngaltiswho/cjahzms2p9f5c2ro2u3yp796s',
   center: [77.5946, 12.9716 ],
-  zoom: 4
+  zoom: 7
 });
 
 map.on('click', function(e) {
@@ -15,11 +15,18 @@ map.on('click', function(e) {
     return;
   }
 
-  var feature = features[0];
+map.on('mousemove', function (e) {
+    map.featuresAt(e.point, {layer: 'aacp-project-information', radius: 10}, function (err, features) {
+        if (err) throw err;
+        map.getCanvas().style.cursor = features.length ? 'pointer' : '';
+    });
+});
 
-  var popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(feature.geometry.coordinates)
-    .setHTML('<h3>' + feature.properties.Title + '</h3><p>' + feature.properties.Description + '</p>')
-    .setLngLat(feature.geometry.coordinates)
-    .addTo(map);
+var feature = features[0];
+
+var popup = new mapboxgl.Popup({ offset: [0, -15] })
+  .setLngLat(feature.geometry.coordinates)
+  .setHTML('<h3>' + feature.properties.Title + '</h3><p>' + feature.properties.Description + '</p>')
+  .setLngLat(feature.geometry.coordinates)
+  .addTo(map);
 });
