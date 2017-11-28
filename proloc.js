20b6fcd7b7bc676f5419a0adccd2f47dd51d4cd2
johngaltiswho <umamaheshwar@aacpinfra.com>
2017-11-28 21:58:22 +0530

    Inckuding Side Bar Second Iteration

--- proloc.js
+++ proloc.js
@@ -6,15 +6,111 @@ var map = new mapboxgl.Map({
   zoom: 9
 });
 
+//Code in order for remove() to work in older browsers
+if (!('remove' in Element.prototype)) {
+  Element.prototype.remove = function() {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  };
+}
+
+map.on('load', function(e) {
+  var features = map.queryRenderedFeatures(e.point, { layers: ['aacp-project-information']});
+  buildProjectLocations(features);
+});
+
+function buildProjectLocations(data) {
+  // Iterate through the list of stores
+  for (i = 0; i < data.length; i++) {
+    var currentFeature = data[i];
+    var prop = currentFeature.properties;
+    // Select the listing container in the HTML and append a div with the class 'item' for each store
+    var listings = document.getElementById('listings');
+    var listing = listings.appendChild(document.createElement('div'));
+    listing.className = 'item';
+    listing.id = 'listing-' + i;
+    // Create a new link with the class 'title' for each store and fill it with the Project Name
+    var link = listing.appendChild(document.createElement('a'));
+    link.href = '#';
+    link.className = 'title';
+    link.dataPosition = i;
+    link.innerHTML = prop.Title;
+    // Create a new div with the class 'details' for each project and fill it with the Descrioption
+    var details = listing.appendChild(document.createElement('div'));
+    details.innerHTML = prop.Description;
+    // Add an event listener for the links in the sidebar listing
+    link.addEventListener('click', function(e) {
+      // Update the currentFeature to the store associated with the clicked link
+      var clickedListing = data[this.dataPosition];
+      // 1. Fly to the point associated with the clicked link
+      flyToProject(clickedListing);
+      // 2. Close all other popups and display popup for clicked store
+      createPopUp(clickedListing);
+      // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+      var activeItem = document.getElementsByClassName('active');
+      if (activeItem[0]) {
+        activeItem[0].classList.remove('active');
+      }
+      this.parentNode.classList.add('active');
+    });
+  }
+}
+
+function flyToProject(currentFeature) {
+  map.flyTo({
+    center: currentFeature.geometry.coordinates,
+    zoom: 15
+  });
+}
+
+function createPopUp(currentFeature) {
+  var popUps = document.getElementsByClassName('mapboxgl-popup');
+  // Check if there is already a popup on the map and if so, remove it
+  if (popUps[0]) popUps[0].remove();
+
+  var popup = new mapboxgl.Popup({ closeOnClick: false })
+    .setLngLat(currentFeature.geometry.coordinates)
+    .setHTML('<h3>' + currentFeature.properties.Title + '</h3><p>' + currentFeature.properties.Description + '</p>')
+    .addTo(map);
+}
+
+// Add an event listener for when a user clicks on the map
+map.on('click', function(e) {
+  // Query all the rendered points in the view
+  var features = map.queryRenderedFeatures(e.point, { layers: ['aacp-project-information'] });
+  if (features.length) {
+    var clickedPoint = features[0];
+    // 1. Fly to the point
+    flyToProject(clickedPoint);
+    // 2. Close all other popups and display popup for clicked store
+    createPopUp(clickedPoint);
+    // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+    var activeItem = document.getElementsByClassName('active');
+    if (activeItem[0]) {
+      activeItem[0].classList.remove('active');
+    }
+    // Find the index of the store.features that corresponds to the clickedPoint that fired the event listener
+    var selectedFeature = clickedPoint.properties.Title;
+
+    for (var i = 0; i < features.length; i++) {
+      if (features[i].properties.Title === selectedFeature) {
+        selectedFeatureIndex = i;
+      }
+    }
+    // Select the correct list item using the found index and add the active class
+    var listing = document.getElementById('listing-' + selectedFeatureIndex);
+    listing.classList.add('active');
+  }
+});
+
 var popup = new mapboxgl.Popup({
     offset: [0, -15],
     closeButton: false
 });
 
-
 map.on('mousemove', function(e) {
   var features = map.queryRenderedFeatures(e.point, { layers: ['aacp-project-information']});
-  //buildProjectLocations('aacp-project-information');
   map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
 
   if (!features.length) {
@@ -27,33 +123,3 @@ map.on('mousemove', function(e) {
     .setLngLat(feature.geometry.coordinates)
     .addTo(map);
 });
-
-function buildProjectLocations(data) {
-  // Iterate through the list of stores
-  for (i = 0; i < data.features.length; i++) {
-    var currentFeature = data.features[i];
-    var prop = currentFeature.properties;
-    // Select the listing container in the HTML and append a div
-    // with the class 'item' for each store
-    var listings = document.getElementById('listings');
-    var listing = listings.appendChild(document.createElement('div'));
-    listing.className = 'item';
-    listing.id = 'listing-' + i;
-
-    // Create a new link with the class 'title' for each store
-    // and fill it with the store address
-    var link = listing.appendChild(document.createElement('a'));
-    link.href = '#';
-    link.className = 'title';
-    link.dataPosition = i;
-    link.innerHTML = prop.address;
-
-    // Create a new div with the class 'details' for each store
-    // and fill it with the city and phone number
-    var details = listing.appendChild(document.createElement('div'));
-    details.innerHTML = prop.city;
-    if (prop.phone) {
-      details.innerHTML += ' &middot; ' + prop.phoneFormatted;
-    }
-  }
-}
